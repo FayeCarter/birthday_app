b45132821ad7032b963725c663935f8ba128181f
Faye Carter <fayecarter333@gmail.com>
2020-04-03 12:16:05 +0100

    Calculator class added

--- spec/features/greeting_spec.rb
+++ spec/features/greeting_spec.rb
@@ -1,9 +1,10 @@
 feature 'Greeting page loads' do
+  
   scenario 'with greeting' do
     visit '/'
     fill_in :name, with: 'Faye'
     click_button 'Go!'
     expect(page).to have_content "Faye"
   end
-
+  
 end
